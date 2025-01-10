6d85dc9fc8cc6369eaa85182f82de663428f4a5c
Dakota Riley <dakotacustompcs@gmail.com>
2025-01-10 17:11:58 -0500

    Add sort_on, sort_characters, and print_report to main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    sorted_chars = sort_characters(chars_dict)
+    #print(sorted_chars)
+    print_report(book_path, num_words, sorted_chars)
 
 def get_book_text(path):
      with open(path) as f:
@@ -22,5 +24,24 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_characters(chars):
+    characters = []
+    for char in chars:
+        if char.isalpha():
+            characters.append({"char": char, "count": chars[char]})
+    characters.sort(reverse=True, key=sort_on)
+    return characters
+
+def print_report(path, words, sorted_chars):
+    print(f"--- Begin report of {path} ---")
+    print(f"{words} words found in the document")
+    print()
+    for char in sorted_chars:
+        print(f"The '{char["char"]}' character was found {char["count"]} times")
+    print("--- End report ---")
 
 main()
